71bf0d227bcd2b9340bdb6e9d3ab14d1180864c5
Maximilian Luz <luzmaximilian@gmail.com>
2022-04-27 16:03:54 +0200

    Copy files instead of using symlinks

--- pkg/debian/updlinks.sh
+++ pkg/debian/updlinks.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh 
 
-rm -rf *.patch
+rm -rf ./*.patch
 
 for f in ../../patches/v1/*.patch; do
-    ln -s $f v1-`basename $f`
+    cp "${f}" "v1-$(basename "$f")"
 done
 
 for f in ../../patches/v2/*.patch; do
-    ln -s $f v2-`basename $f`
+    cp "${f}" "v2-$(basename "$f")"
 done
